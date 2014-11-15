c3fa0fecbec16da0e28e8658be32c876d5e6b707
Ikuo Degawa <degawa@ikuwow.com>
2014-11-15 17:11:19 +0900

    include default script in testInpainting.m

--- testInpainting.m
+++ testInpainting.m
@@ -1,25 +1,32 @@
-% testInpainting.m
-% 
-% The test (demo) script for inpainting.m
-
 clear
 
-%% parameters section %% 
-TestImgName = 'lena';
-psz = 15; % patch size
+testImageName = 'cow'; % cow or bungee or man
+psz = 15; % patch size ( to be inpainted in inpainting)
 
-inputImg = imread('lena.bmp','BMP'); % uint8
+testImagePath = '~/Documents/MATLAB/AutoShared/testimages/Petter_Strandmark/';
+testImageSource = fullfile(testImagePath,testImageName);
 
-
-mask = imread('mask.bmp','BMP'); % uint8
-
+img = imread([testImageSource,'.png']);
+mask = imread([testImageSource,'-mask.png']);
 mask(mask==255) = 1;
-maskedImg = mask.*inputImg;
 
-outputImg = inpainting(inputImg, mask, psz);
-outputImg = uint8(outputImg);
+Rimg = img(:,:,1); Rimg(mask==1) = 0; img(:,:,1) = Rimg;
+Gimg = img(:,:,2); Gimg(mask==1) = 255; img(:,:,2) = Gimg;
+Bimg = img(:,:,3); Bimg(mask==1) = 0; img(:,:,3) = Bimg;
 
-figure(1), imshow(inputImg)
-figure(2), imshow(maskedImg)
-figure(3), imshow(outputImg)
+tmpImgFileName = [testImageName,'_masked.bmp'];
+imwrite(img,tmpImgFileName,'BMP');
 
+imgFilename = testImageSource;
+fillFilename = tmpImgFileName;
+tic
+[i1,i2,i3,c,d,mov] = inpainting([imgFilename,'.png'],fillFilename,[0 255 0]);
+toc
+
+figure(1),imshow(uint8(i2)),title('Masked Image')
+figure(2),imshow(uint8(i1)),title('Inpainted Image')
+folderName = ['myresults/',datestr(now,'yymmdd-HHMMSS'),'_',testImageName];
+mkdir(folderName)
+
+imwrite(uint8(i2),fullfile(folderName,'maskedImg.bmp'),'BMP');
+imwrite(uint8(i1),fullfile(folderName,'inpaintedImg.bmp'),'BMP');
